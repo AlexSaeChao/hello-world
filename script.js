829b79fc58788245eb604890cd55877b2df83bc4
Alex Chao <chaoalex93@gmail.com>
2023-04-20 00:10:16 +0000

    colors added to section for making frames

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ document.write("Hi " + userName +", it's nice of you to be here.")
 
 //if (this is true) {execute this code}
 if (userName == "") {
-    document.write("Hi stranger, it's nice of you to be here.");
+    document.write(" Hi stranger, it's good to be here.");
 } else if (userName == null) {
     document.write(" You've pressed the cancel button, that's okay.");
 } else {
-    document.write("You have your own name!?")
+    document.write(" You have your own name!?")
 }
